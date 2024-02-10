9431934892af98c4912b9fa58bef3554da2f92f1
Christian Lang <christian.lang92@gmx.de>
2024-02-10 15:22:43 +0100

    VERSION 2024-02.1

--- version.js
+++ version.js
@@ -2,9 +2,9 @@
 const state_description_dict_1 = require("./state_description/1").state_description_dict;
 
 //ON_RELEASE: CHANGE EVERY RELEASE
-global.VERSION_YEAR = 2023;
-global.VERSION_MONTH = 11;
-global.VERSION_NUMBER = 2;
+global.VERSION_YEAR = 2024;
+global.VERSION_MONTH = 2;
+global.VERSION_NUMBER = 1;
 //ON_RELEASE: INCREMENT IF STATE DATA DESCRIPTION CHANGES
 global.STATE_VERSION = 1;
 
